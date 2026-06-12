7936417448d91ab9b09c5915c996137fe9b02afd
asama7lmy <omarsalahh120@gmail.com>
2026-06-13 02:12:39 +0300

    remove generateStaticParams

--- src/app/[locale]/admin/menu-items/[productId]/edit/page.tsx
+++ src/app/[locale]/admin/menu-items/[productId]/edit/page.tsx
@@ -1,16 +1,56 @@
+// import { Pages, Routes } from "@/constants/enums";
+// import { Locale } from "@/i18n.config";
+// import { getProduct, getProducts } from "@/server/db/products";
+// import { redirect } from "next/navigation";
+// import Form from "../../_components/Form";
+// import { getCategories } from "@/server/db/categories";
+// import getTrans from "@/lib/translation";
+// export const dynamic = "force-dynamic";
+// export async function generateStaticParams() {
+//   const products = await getProducts();
+
+//   return products.map((product) => ({ productId: product.id }));
+// }
+// async function EditProductPage({
+//   params,
+// }: {
+//   params: Promise<{ locale: Locale; productId: string }>;
+// }) {
+//   const { productId, locale } = await params;
+//   const translations = await getTrans(locale);
+//   const product = await getProduct(productId);
+//   const categories = await getCategories();
+
+//   if (!product) {
+//     redirect(`/${locale}/${Routes.ADMIN}/${Pages.MENU_ITEMS}`);
+//   }
+
+//   return (
+//     <main>
+//       <section>
+//         <div className="container">
+//           <Form
+//             categories={categories}
+//             translations={translations}
+//             product={product}
+//           />
+//         </div>
+//       </section>
+//     </main>
+//   );
+// }
+
+// export default EditProductPage;
 import { Pages, Routes } from "@/constants/enums";
 import { Locale } from "@/i18n.config";
-import { getProduct, getProducts } from "@/server/db/products";
+import { getProduct } from "@/server/db/products";
 import { redirect } from "next/navigation";
 import Form from "../../_components/Form";
 import { getCategories } from "@/server/db/categories";
 import getTrans from "@/lib/translation";
 
-export async function generateStaticParams() {
-  const products = await getProducts();
+export const dynamic = "force-dynamic";
 
-  return products.map((product) => ({ productId: product.id }));
-}
 async function EditProductPage({
   params,
 }: {
@@ -18,6 +58,7 @@ async function EditProductPage({
 }) {
   const { productId, locale } = await params;
   const translations = await getTrans(locale);
+
   const product = await getProduct(productId);
   const categories = await getCategories();
 
